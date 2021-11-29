9e96939c610e07e0420afd6b6c83123ab710d81f
gdeignacio-fundaciobit <gdeignacio@fundaciobit.org>
2021-11-29 23:49:10 +0100

    Backoffice impl #2

---
+++ emiservbackoffice-ws/emiservbackoffice_ws_server/src/main/java/es/caib/emiservbackoffice/ws/impl/EmiservBackofficeWsImpl.java
@@ -0,0 +1,54 @@
+package es.caib.emiservbackoffice.ws.impl;
+
+import es.caib.emiserv.logic.intf.service.ws.backoffice.ConfirmacionPeticion;
+import es.caib.emiserv.logic.intf.service.ws.backoffice.EmiservBackoffice;
+import es.caib.emiserv.logic.intf.service.ws.backoffice.Peticion;
+import es.caib.emiserv.logic.intf.service.ws.backoffice.Respuesta;
+import es.caib.emiserv.logic.intf.service.ws.backoffice.SolicitudRespuesta;
+import es.caib.emiservbackoffice.ws.utils.BaseWsImpl;
+import es.caib.emiservbackoffice.ws.utils.WsI18NException;
+
+import javax.ejb.Stateless;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+
+/**
+ * @author gdeignacio
+ */
+@Stateless(name = EmiservBackofficeWsImpl.NAME + "Ejb")
+@SOAPBinding(style = SOAPBinding.Style.RPC)
+@WebService(
+        name = EmiservBackofficeWsImpl.NAME_WS,
+        portName = EmiservBackofficeWsImpl.NAME_WS,
+        serviceName = EmiservBackofficeWsImpl.NAME_WS + "Service",
+        targetNamespace = "http://caib.es/emiserv/backoffice")
+public class EmiservBackofficeWsImpl extends BaseWsImpl implements EmiservBackoffice {
+
+    public static final String NAME = "EmiservBackoffice";
+
+    public static final String NAME_WS = NAME + "Ws";
+
+    @WebMethod
+    public String echo(@WebParam(name = "echo") String echo) throws WsI18NException {
+        log.info("EmiservBackofficeWsImpl :: echo = " + echo);
+        return echo;
+    }
+
+    @Override
+    public Respuesta peticionSincrona(Peticion peticion) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ConfirmacionPeticion peticionAsincrona(Peticion peticion) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Respuesta solicitarRespuesta(SolicitudRespuesta solicitudRespuesta) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
