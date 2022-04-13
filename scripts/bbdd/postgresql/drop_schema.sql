e414960e08bf6008446998ff42aeac6820af7943
gdeignacio-fundaciobit <gdeignacio@fundaciobit>
2022-04-13 13:36:34 +0200

    Excloure sentències de eliminar schema a la inicialització #6

--- scripts/bbdd/postgresql/drop_schema.sql
+++ scripts/bbdd/postgresql/drop_schema.sql
@@ -1,11 +1,11 @@
 
-    alter table EBO_PROCEDIMENT 
-       drop constraint EBO_PROCEDIMENT_UNITAT_FK;
+    --alter table EBO_PROCEDIMENT 
+    --   drop constraint EBO_PROCEDIMENT_UNITAT_FK;
 
-    drop table if exists EBO_PROCEDIMENT cascade;
+    --drop table if exists EBO_PROCEDIMENT cascade;
 
-    drop table if exists EBO_UNITATORGANICA cascade;
+    --drop table if exists EBO_UNITATORGANICA cascade;
 
-    drop sequence if exists EBO_PROCEDIMENT_SEQ;
+    --drop sequence if exists EBO_PROCEDIMENT_SEQ;
 
-    drop sequence if exists EBO_UNITATORGANICA_SEQ;
+    --drop sequence if exists EBO_UNITATORGANICA_SEQ;
